e799c692e7df3b7b223c76787619975765549ddb
jameson-dev <178156579+jameson-dev@users.noreply.github.com>
2025-01-09 23:12:55 +1030

    Implement multipart audio streaming

--- server/server.py
+++ server/server.py
@@ -88,25 +88,54 @@ class FileHandler(FileSystemEventHandler):
         base_name = os.path.splitext(os.path.basename(rfa_file_path))[0]
 
         # Extract the meaningful part of the filename (before the timestamp and ID)
-        match = re.match(r"([a-zA-Z0-9_]+)_.*", base_name)
+        match = re.match(r"(P[1-3])_([a-zA-Z0-9_]+)_.*", base_name)
         if not match:
             logger.error(f"Filename format is unrecognized: {base_name}")
             return
 
-        keyword_part = match.group(1)  # Extract the part before the timestamp
+        keyword_part = match.group(2)  # Extract the part after the priority
         normalized_keyword = keyword_part.replace("_", " ").lower().strip()
 
+        # Check for Priority in filename:
+        incident_priority = base_name[:2]   # Get P1, P2 or P3 from the start of the filename
+
+        logger.info(f"Priority: {incident_priority}, Incident: {keyword_part}")
+
         # Replace spaces with underscores for .wav file matching
-        wav_file_name = normalized_keyword.replace(" ", "_") + ".wav"
+        inc_type_wav = normalized_keyword.replace(" ", "_") + ".wav"
+        incident_priority_wav = f"{incident_priority}.wav"
 
-        audio_file_path = os.path.join(self.audio_files_folder, wav_file_name)
+        audio_file_path = os.path.join(self.audio_files_folder, inc_type_wav)
+        inc_priority_audio_file_path = os.path.join(self.audio_files_folder, incident_priority_wav)
 
-        if os.path.exists(audio_file_path):
+        if os.path.exists(audio_file_path) and os.path.exists(inc_priority_audio_file_path):
             logger.debug(f"Found audio file {audio_file_path}")
-            logger.info(f"Streaming {audio_file_path}")
+            logger.debug(f"Found priority audio file {inc_priority_audio_file_path}")
+            logger.info(f"Streaming {inc_priority_audio_file_path} and {audio_file_path}")
+            self.stream_audio_sequentially(audio_file_path, inc_priority_audio_file_path)
+        # If priority wav file not found, just stream incident type wav
+        elif os.path.exists(audio_file_path) and not os.path.exists(inc_priority_audio_file_path):
+            logger.warning(f"Incident Priority ({inc_priority_audio_file_path}) could not be found. Only playing incident type ({audio_file_path})")
             self.stream_audio(audio_file_path)
         else:
             logger.error(f"Error: Audio file '{audio_file_path}' not found.")
+
+    def stream_audio_sequentially(self, audio_file_path, priority_audio_path):
+        """Stream two audio files sequentially to the client."""
+        try:
+            logger.info(f"Streaming audio files")
+
+            # Send the incident priority audio file
+            self.stream_audio(priority_audio_path)
+
+            # Send the incident type audio file
+            self.stream_audio(audio_file_path)
+
+
+
+            logger.info("Both audio files streamed successfully.")
+        except FileNotFoundError as e:
+            logger.error("Error streaming audio: {e}")
 
     def stream_audio(self, file_path):
         try:
